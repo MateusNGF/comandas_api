f48a929f9652f5405dd1e27448ca2a36e7040ff7
Mateus Nicolau <mateus.nicolau@devmagic.com.br>
2022-03-21 14:03:41 -0300

    fix(*): remove file teste

--- src/controllers/commands/teste.ts
+++
@@ -1,8 +0,0 @@
-var produtos = [
-  { id: 1, nome: "bhama" },
-  { id : 2, nome: "skol"}
-]
-
-let index = produtos.indexOf(produtos.find(p => p.id === 1))
-produtos.splice(index, 1)
-console.log(produtos)

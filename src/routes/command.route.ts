79438e3c172d7d358fa3dd60d50ec4e3c1abd967
Mateus Nicolau <mateus.nicolau@devmagic.com.br>
2022-04-02 18:45:19 -0300

    fix(*): add summary in command route

--- src/routes/command.route.ts
+++ src/routes/command.route.ts
@@ -5,27 +5,31 @@ import { ExpressAdapterRouter, verify } from '../utils'
 export const command_routes = Express.Router()
 
 command_routes.post("/:eventId/criar", verify,
-   /**
-    * #swagger.tags = ["Comandas"]
-    */
+  /**
+    #swagger.tags = ["Comandas"]
+    #swagger.summary = "Cria uma comanda para o evento referenciado."
+  */
   ExpressAdapterRouter.adapt(new CreateCommand()))
 
 command_routes.post("/:eventId/:commandId/:action/produto/:productId", verify, 
-   /**
-    * #swagger.tags = ["Comandas"]
-    */
+  /**
+    #swagger.tags = ["Comandas"]
+    #swagger.summary = "Adicionar ou Remove um produto na comanda."
+  */
   ExpressAdapterRouter.adapt(new ManagerProductInCommand()))
 
 command_routes.put("/:eventId/:commandId/pagar", verify, 
-   /**
-    * #swagger.tags = ["Comandas"]
-    */
+  /**
+    #swagger.tags = ["Comandas"]
+    #swagger.summary = "Efetua o pagamento da comanda."
+  */
   ExpressAdapterRouter.adapt(new PayCommand()))
 
 command_routes.get("/:eventId/:commandId/pegar", verify, 
-   /**
-    * #swagger.tags = ["Comandas"]
-    */
+  /**
+    #swagger.tags = ["Comandas"]
+    #swagger.summary = "Pega uma comanda e tras toda sua informação."
+  */
   ExpressAdapterRouter.adapt(new GetCommand()))
 
 
